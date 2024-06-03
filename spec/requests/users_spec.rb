9504ec55ed65699476830e1c755773ad679b3adf
Yo Kamada <bumpfuji10@gmail.com>
2024-06-04 08:32:38 +0900

    add: request specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,47 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/users/index"
-      expect(response).to have_http_status(:success)
+RSpec.describe UsersController, type: :request do
+  describe "index" do
+    it "ステータスコード200" do
+      get("/users")
+      expect(response.status).to eq 200
     end
   end
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/users/new"
-      expect(response).to have_http_status(:success)
+  describe "new" do
+    it "ステータスコード200" do
+      get("/users/new")
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe "show" do
+    let(:user) { FactoryBot.create(:user) }
+    it "ステータスコード200" do
+      get("/users/#{user.id}")
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe "create" do
+    let(:params) {
+      {
+        user: {
+          name: "testくん",
+          email: "test@testhoge.com",
+          password: "password"
+        }
+      }
+    }
+    let(:request) { post("/users", params: params) }
+
+    it "ステータスコード201" do
+      request
+      expect(response.status).to eq 201
+    end
+
+    it "Userインスタンス1増加" do
+      expect { request }.to change { User.count }.by(1)
     end
   end
 
