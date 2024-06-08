9b1d67f0dca57a2e761a4f40a1dffd8b8141d3d5
Yo Kamada <bumpfuji10@gmail.com>
2024-06-09 01:03:30 +0900

    add: relations and validates

--- app/models/practice_record.rb
+++ app/models/practice_record.rb
@@ -1,5 +1,6 @@
 class PracticeRecord < ApplicationRecord
   belongs_to :user
+  has_many :practice_record_details
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :practiced_date, presence: true
--- app/models/practice_record_detail.rb
+++ app/models/practice_record_detail.rb
@@ -1,2 +1,7 @@
 class PracticeRecordDetail < ApplicationRecord
+  belongs_to :practice_record
+
+  validates :activity_title, presence: true, length: { maximum: 100 }
+  validates :content, presence: true, length: { maximum: 10000 }
+
 end
