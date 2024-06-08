3e823ef7c7ee59ffc2500b060d9a2e45b9511561
Yo Kamada <bumpfuji10@gmail.com>
2024-06-09 08:42:54 +0900

    add: about content specs

--- spec/models/practice_record_detail_spec.rb
+++ spec/models/practice_record_detail_spec.rb
@@ -28,6 +28,81 @@ RSpec.describe PracticeRecordDetail, type: :model do
           expect(practice_record_detail).to be_invalid
         end
       end
+
+      context "長さ100文字" do
+        let(:practice_record_detail) { FactoryBot.build(:practice_record_detail, activity_title: "a" * 100) }
+
+        it "保存成功" do
+          expect(practice_record_detail.save).to eq true
+        end
+
+        it "有効" do
+          expect(practice_record_detail).to be_valid
+        end
+      end
+
+      context "長さ101文字" do
+        let(:practice_record_detail) { FactoryBot.build(:practice_record_detail, activity_title: "a" * 101) }
+
+        it "保存失敗" do
+          expect(practice_record_detail.save).to eq false
+        end
+
+        it "無効" do
+          expect(practice_record_detail).to be_invalid
+        end
+      end
+    end
+
+    context "content" do
+
+      context "contentあり" do
+        let(:practice_record_detail) { FactoryBot.build(:practice_record_detail, content: "いい感じでした") }
+
+        it "保存成功" do
+          expect(practice_record_detail.save).to eq true
+        end
+
+        it "有効" do
+          expect(practice_record_detail).to be_valid
+        end
+      end
+
+      context "contentなし" do
+        let(:practice_record_detail) { FactoryBot.build(:practice_record_detail, content: nil) }
+
+        it "保存失敗" do
+          expect(practice_record_detail.save).to eq false
+        end
+
+        it "無効" do
+          expect(practice_record_detail).to be_invalid
+        end
+      end
+
+      context "長さ10000文字" do
+        let(:practice_record_detail) { FactoryBot.build(:practice_record_detail, content: "a" * 10000) }
+
+        it "保存成功" do
+          expect(practice_record_detail.save).to eq true
+        end
+
+        it "有効" do
+          expect(practice_record_detail).to be_valid
+        end
+      end
+
+      context "長さ10001文字" do
+        let(:practice_record_detail) { FactoryBot.build(:practice_record_detail, content: "a" * 10001) }
+
+        it "保存失敗" do
+          expect(practice_record_detail.save).to eq false
+        end
+
+        it "無効" do
+          expect(practice_record_detail).to be_invalid
+        end
+      end
     end
   end
 end
