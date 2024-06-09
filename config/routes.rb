0fcd756334a5271c2716efef4d78dc288da4427a
Yo Kamada <bumpfuji10@gmail.com>
2024-06-09 19:11:00 +0900

    ベーシックなログイン画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "users#new"
   resources :users, only: [:new, :create, :show, :index]
   resources :practice_records
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
 end
