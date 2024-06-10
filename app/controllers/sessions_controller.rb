ab2a341c813651301fb8ba2689410cbc5ae758b9
Yo Kamada <bumpfuji10@gmail.com>
2024-06-11 00:12:37 +0900

    change: ログアウト後のリダイレクト先をusers/newに変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -33,6 +33,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "ログアウトしました"
+    redirect_to login_path, notice: "ログアウトしました"
   end
 end
