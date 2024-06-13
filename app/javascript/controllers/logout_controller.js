656186741acecf95eb0651b657a19699ba97fbcd
Yo Kamada <bumpfuji10@gmail.com>
2024-06-13 23:59:50 +0900

    ログアウト処理を行うコントローラを追加

---
+++ app/javascript/controllers/logout_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  logout(event) {
+    event.preventDefault() // デフォルトのリンク動作を防止
+    const result = confirm("ログアウトしますか？")
+    if (result) {
+      // JSを使ってフォームを作成し送信
+      const form = document.createElement("form")
+      form.method = "POST"
+      form.action = this.element.href
+
+      const csrfToken = document.querySelector("meta[name=csrf-token]").content
+      const methodInput = document.createElement("input")
+      methodInput.type = "hidden"
+      methodInput.name = "_method"
+      methodInput.value = "delete"
+
+      const csrfInput = document.createElement("input")
+      csrfInput.type = "hidden"
+      csrfInput.name = "authenticity_token"
+      csrfInput.value = csrfToken
+
+      form.appendChild(methodInput)
+      form.appendChild(csrfInput)
+      document.body.appendChild(form)
+
+      form.submit()
+    }
+  }
+}
