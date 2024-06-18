5e1dd429514c504bcf89004558aa019535368820
Yo Kamada <bumpfuji10@gmail.com>
2024-06-19 00:43:28 +0900

    一旦wipで練習記録機能実装

--- app/controllers/practice_records_controller.rb
+++ app/controllers/practice_records_controller.rb
@@ -1,14 +1,39 @@
 class PracticeRecordsController < ApplicationController
+  before_action :set_user
 
   def index
     @practice_records = PracticeRecord.all
   end
 
-  def new
+  def show
+    @practice_record = PracticeRecord.find(params[:id])
+  end
 
+  def new
+    @practice_record = PracticeRecord.new
+    @practice_record.practice_record_details.build
   end
 
   def create
+    @practice_record = @user.practice_records.new(practice_record_params)
+    if @practice_record.save
+      redirect_to(practice_records_path, notice: "練習記録を作成しました")
+    else
+      render(:new, status: :unprocessable_entity)
+    end
+  end
 
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def practice_record_params
+    params.require(:practice_record).permit(
+      :title,
+      :practiced_date,
+      practice_record_details_attributes: [:id, :activity_title, :content, :_destroy]
+    )
   end
 end
