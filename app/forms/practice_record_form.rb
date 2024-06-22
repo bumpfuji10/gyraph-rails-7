5f8d42c2dfbfefab4fe09e1edba5f87981cc0806
Yo Kamada <bumpfuji10@gmail.com>
2024-06-22 17:51:17 +0900

    [change]userオブジェクトをコンストラクタに含めて、saveメソッドを追加

--- app/forms/practice_record_form.rb
+++ app/forms/practice_record_form.rb
@@ -2,7 +2,7 @@ class PracticeRecordForm
   # ActiveModel::Modelをincludeすることで、バリデーションやエラーメッセージの機能を使えるようになる
   include ActiveModel::Model
 
-  attr_accessor :title, :practiced_date, :practice_record_details_attributes
+  attr_accessor :user, :title, :practiced_date, :practice_record_details_attributes
 
   validates :title, presence: true
   validates :practiced_date, presence: true
@@ -10,6 +10,7 @@ class PracticeRecordForm
 
   # MEMO
   # attributes = {
+  #   user: User.first,
   #   title: "New Practice Record",
   #   practiced_date: Date.today,
   #   practice_record_details_attributes: [
@@ -20,8 +21,27 @@ class PracticeRecordForm
   def initialize(attributes = {})
     # attributesにはフォームから送信されたデータが入る
 
+    @user = attributes[:user]
     @title = attributes[:title]
     @practiced_date = attributes[:practiced_date]
     @practice_record_details_attributes = attributes[:practice_record_details_attributes]
+  end
+
+  def save
+    return false if invalid?
+    practice_record = PracticeRecord.new(
+      user: user,
+      title: title,
+      practiced_date: practiced_date
+    )
+
+    practice_record_details_attributes.each do |detail_attributes|
+      practice_record.practice_record_details.build(
+        activity_title: detail_attributes[:activity_title],
+        content: detail_attributes[:content]
+      )
+    end
+
+    practice_record.save
   end
 end
