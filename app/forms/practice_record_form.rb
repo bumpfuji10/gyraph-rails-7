84382b2bc3a0467c3e7d69ad5aa7fd111a23e7d4
Yo Kamada <bumpfuji10@gmail.com>
2024-06-22 17:28:01 +0900

    [change]PraciticeRecordのtitleとpracticed_dateと、PracticeRecordDetailのattributeを、コンストラクタで受け取り、操作できるように修正。また、validationを追加。

--- app/forms/practice_record_form.rb
+++ app/forms/practice_record_form.rb
@@ -1,3 +1,27 @@
 class PracticeRecordForm
+  # ActiveModel::Modelをincludeすることで、バリデーションやエラーメッセージの機能を使えるようになる
   include ActiveModel::Model
+
+  attr_accessor :title, :practiced_date, :practice_record_details_attributes
+
+  validates :title, presence: true
+  validates :practiced_date, presence: true
+  validates :practice_record_details_attributes, presence: true
+
+  # MEMO
+  # attributes = {
+  #   title: "New Practice Record",
+  #   practiced_date: Date.today,
+  #   practice_record_details_attributes: [
+  #     { activity_title: "Running", content: "5 km" },
+  #     { activity_title: "Swimming", content: "1 km" }
+  #   ]
+  # }
+  def initialize(attributes = {})
+    # attributesにはフォームから送信されたデータが入る
+
+    @title = attributes[:title]
+    @practiced_date = attributes[:practiced_date]
+    @practice_record_details_attributes = attributes[:practice_record_details_attributes]
+  end
 end
