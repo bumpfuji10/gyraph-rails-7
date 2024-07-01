0db72a07d08bbbdca3041f3d9ea1484ced14dfc5
Yo Kamada <bumpfuji10@gmail.com>
2024-07-02 08:26:05 +0900

    [change]tailwindが即時反映されるように

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,8 +56,15 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Tailwind CSS configuration
+  config.assets.debug = true
+  config.assets.compile = true
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+
+  # ファイル変更の監視を有効にする
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
