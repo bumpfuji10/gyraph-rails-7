2253af8663ad3452b0012ded65eba34e673e34a1
Yo Kamada <bumpfuji10@gmail.com>
2024-07-06 21:14:23 +0900

    [change]一旦フォームを追加できるように実装

--- app/javascript/controllers/append_form_field_controller.js
+++ app/javascript/controllers/append_form_field_controller.js
@@ -1,9 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["button"]
+  static targets = ["fieldsContainer"]
 
-  append() {
-    console.log("hoge")
+  append(event) {
+    event.preventDefault();
+
+    const template = this.cloneDetailFormTemplate()
+    const newIndex = new Date().getTime().toString();
+    this.replaceNewFormIndex(template, newIndex)
+  }
+
+  // フォームテンプレートを複製
+  cloneDetailFormTemplate() {
+    return document.getElementById('practice-record-details-template').content.cloneNode(true);
+  }
+
+  // フォームテンプレートをDOMに追加
+  replaceNewFormIndex(template, newIndex) {
+    template.querySelectorAll("input, textarea").forEach(input => {
+      input.name = input.name.replace(/NEW_FORM/g, newIndex);
+    });
+    return this.fieldsContainerTarget.appendChild(template);
   }
 }
