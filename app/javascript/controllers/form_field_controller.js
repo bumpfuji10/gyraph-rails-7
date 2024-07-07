88decd767607d0abf2f1477ba3d407ad432083ca
Yo Kamada <bumpfuji10@gmail.com>
2024-07-07 19:23:31 +0900

    [change]関数名の変更

--- app/javascript/controllers/form_field_controller.js
+++ app/javascript/controllers/form_field_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["fieldsContainer"]
 
-  append(event) {
+  appendForm(event) {
     event.preventDefault();
 
     const template = this.cloneDetailFormTemplate()
