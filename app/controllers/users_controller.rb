b4b3df7946dfc57354871bd8cf90ea4ec4a7f0bb
Yo Kamada <bumpfuji10@gmail.com>
2024-08-04 17:21:54 +0900

    [add]メールテキストの「こちら」をクリックすると発火するエンドポイント、confirmアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,24 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    UserMailer.confirm(@user).deliver_now
     if @user.save
-      redirect_to(practice_records_path)
+      UserMailer.confirm(@user).deliver_now
+      redirect_to(login_path)
+      flash[:success] = "アカウントの仮登録が完了しました。メールを確認してアカウントを有効化してください。"
     else
       render(:new, status: :unprocessable_entity)
+    end
+  end
+
+  def confirm
+    user = User.find_by(confirmation_token: params[:token])
+    if user.present? &&  user.confirmation_sent_at > 24.hours.ago
+      user.confirm
+      redirect_to(login_path)
+      flash[:success] = "アカウントを有効化しました。お手数ですが、再度ログインしてください。"
+    else
+      render(:new, status: :unprocessable_entity)
+      flash.now[:alert] = "アカウントの有効化に失敗しました"
     end
   end
 
