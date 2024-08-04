c16a4c57c30bd79797009f6ff3653bd15ccb3069
Yo Kamada <bumpfuji10@gmail.com>
2024-08-04 17:19:26 +0900

    [add]token認証用のURLを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root "pages#home"
   get "/signup", to: "users#new"
   resources :users, only: [:new, :create, :show, :index, :edit, :update]
+  get "/account_confirmation/:token", to: "users#confirm", as: "account_confirmation"
   resources :practice_records
 
   get "login", to: "sessions#new"
