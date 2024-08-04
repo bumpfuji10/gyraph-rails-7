533ba3fd160d449c9b49d168703a49bcf9b971b1
Yo Kamada <bumpfuji10@gmail.com>
2024-08-04 17:10:38 +0900

    [add]confirmation_token, confirmed_at, confirmation_sent_at to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_21_091911) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_01_150143) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -65,6 +65,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_21_091911) do
     t.datetime "updated_at", null: false
     t.string "password_digest", null: false
     t.text "profile"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
