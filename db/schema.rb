a69fd13c9c89bbaa851ebe3dea4b16e3002a8b50
Yo Kamada <bumpfuji10@gmail.com>
2024-10-01 00:06:39 +0900

    [change]migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_08_17_092437) do
-  create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_17_092437) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8mb4", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,14 +33,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_17_092437) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", force: :cascade do |t|
+  create_table "active_storage_variant_records", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "practice_record_details", force: :cascade do |t|
-    t.integer "practice_record_id", null: false
+  create_table "practice_record_details", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "practice_record_id", null: false
     t.string "activity_title", null: false
     t.string "content", null: false
     t.datetime "created_at", null: false
@@ -48,8 +48,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_17_092437) do
     t.index ["practice_record_id"], name: "index_practice_record_details_on_practice_record_id"
   end
 
-  create_table "practice_records", force: :cascade do |t|
-    t.integer "user_id", null: false
+  create_table "practice_records", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "title", null: false
     t.date "practiced_date", null: false
     t.datetime "created_at", null: false
@@ -58,8 +58,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_17_092437) do
     t.index ["user_id"], name: "index_practice_records_on_user_id"
   end
 
-  create_table "user_password_resets", force: :cascade do |t|
-    t.integer "user_id", null: false
+  create_table "user_password_resets", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "reset_password_token", null: false
     t.datetime "reset_password_sent_at", null: false
     t.datetime "created_at", null: false
@@ -68,7 +68,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_17_092437) do
     t.index ["user_id"], name: "index_user_password_resets_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
     t.datetime "created_at", null: false
