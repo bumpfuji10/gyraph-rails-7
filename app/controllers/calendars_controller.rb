3caa323846f7180edebbf26ce8a4f8800464426f
Yo Kamada <bumpfuji10@gmail.com>
2024-10-10 00:28:00 +0900

    [change]calendarsのエンドポイントを用意してfetchする

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,12 +1,14 @@
 class CalendarsController < ApplicationController
   def show
-    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @date = params[:date].present? ? Date.parse(params[:date]) : Date.today
     @practice_records = PracticeRecord.where(practiced_date: @date.all_month)
 
     respond_to do |format|
-      format.html
-      format.turbo_stream do
+      format.html do
         render partial: "shared/calendar", locals: { date: @date, practice_records: @practice_records }
+      end
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace('calendar', partial: "shared/calendar", locals: { date: @date, practice_records: @practice_records })
       end
     end
   end
--- app/javascript/controllers/calendar_modal_controller.js
+++ app/javascript/controllers/calendar_modal_controller.js
@@ -1,10 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["modal", "overlay"]
+  static targets = ["modal", "overlay", "calendarContent"]
 
   openCalendar() {
     this.overlayTarget.classList.remove('hidden');
+
+    fetch("/calendars")
+    .then(response => response.text())
+    .then(html => {
+      this.calendarContentTarget.innerHTML = html;
+    })
+    .catch(error => {
+      console.error(error);
+    });
   }
 
   closeCalendar() {
