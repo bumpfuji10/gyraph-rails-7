ad3e933bd3147db8dd337257320804e011862cf6
Yo Kamada <bumpfuji10@gmail.com>
2024-10-10 19:59:56 +0900

    [iwip]一旦カレンダーが動くように

--- app/javascript/controllers/calendar_modal_controller.js
+++ app/javascript/controllers/calendar_modal_controller.js
@@ -3,20 +3,29 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["modal", "overlay", "calendarContent"]
 
+  // コントローラーのインスタンスプロパティとして closeCalendar メソッドを定義
+  initialize() {
+    this.boundCloseCalendar = this.closeCalendar.bind(this);
+  }
+
   openCalendar() {
     this.overlayTarget.classList.remove('hidden');
 
     fetch("/calendars")
-    .then(response => response.text())
-    .then(html => {
-      this.calendarContentTarget.innerHTML = html;
-    })
-    .catch(error => {
-      console.error(error);
-    });
+      .then(response => response.text())
+      .then(html => {
+        this.calendarContentTarget.innerHTML = html;
+        document.addEventListener("click", this.boundCloseCalendar);
+      })
+      .catch(error => {
+        console.error("Error loading calendar:", error);
+      });
   }
 
-  closeCalendar() {
-    this.overlayTarget.classList.add('hidden');
+  closeCalendar(event) {
+    if (!this.calendarContentTarget.contains(event.target)) {
+      this.overlayTarget.classList.add('hidden');
+      document.removeEventListener("click", this.boundCloseCalendar);
+    }
   }
 }
