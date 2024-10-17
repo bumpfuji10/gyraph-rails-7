6e3ee8d0bdf73babbf53ca0f2772bdfc9569469d
Yo Kamada <bumpfuji10@gmail.com>
2024-10-17 18:52:15 +0900

    [add]relationship

--- app/models/practice_record.rb
+++ app/models/practice_record.rb
@@ -1,6 +1,8 @@
 class PracticeRecord < ApplicationRecord
   belongs_to :user
   has_many :practice_record_details, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
   accepts_nested_attributes_for :practice_record_details, allow_destroy: true
 
   validates :title, presence: true, length: { maximum: 100 }
