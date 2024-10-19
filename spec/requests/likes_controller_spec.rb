9ff2e5755654da674a45a485967fae1cc183fee3
Yo Kamada <bumpfuji10@gmail.com>
2024-10-19 23:44:14 +0900

    [add]likes endpoint request spec skeleton

---
+++ spec/requests/likes_controller_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe LikesController, type: :request do
+  describe "POST /practice_records/:id/likes" do
+
+  end
+
+  describe "DELETE /practice_records/:id/likes/:id" do
+
+  end
+end
