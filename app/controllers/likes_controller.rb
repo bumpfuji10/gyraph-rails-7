46adf8524c105f40681c3a63cb54fdc673d8a159
Yo Kamada <bumpfuji10@gmail.com>
2024-10-19 11:00:12 +0900

    [change]turbo-frameを置く場所をcomponentに変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,31 +1,45 @@
 class LikesController < ApplicationController
+  before_action :set_practice_record, only: [:create, :destroy]
 
   def index
 
   end
 
   def create
-    @practice_record = PracticeRecord.find(params[:practice_record_id])
-    like = Like.new(user: current_user, practice_record: @practice_record)
+    @like = current_user.likes.create!(practice_record: @practice_record)
 
-    if like.save
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "like_button_#{@practice_record.id}",
+          partial: "shared/like_button",
+          locals: { practice_record: @practice_record, liked: true, like: @like }
+        )
+      end
+    end
+  end
+
+  def destroy
+    like = @practice_record.likes.find(params[:id])
+
+    if like.destroy
       respond_to do |format|
         format.turbo_stream do
           render turbo_stream: turbo_stream.replace(
             "like_button_#{@practice_record.id}",
             partial: "shared/like_button",
-            locals: { practice_record: @practice_record, liked: true }
+            locals: { practice_record: @practice_record, liked: false }
           )
         end
       end
     else
-      flash[:alert] = "いいねに失敗しました"
-      redirect_to @practice_record
+      redirect_to @practice_record, alert: "いいねの取り消しに失敗しました"
     end
-
   end
 
-  def destroy
+  private
 
+  def set_practice_record
+    @practice_record = PracticeRecord.find(params[:practice_record_id])
   end
 end
