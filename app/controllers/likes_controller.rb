3b8ad16ff28cae6b8fa0effbcad7f9709ec9e653
Yo Kamada <bumpfuji10@gmail.com>
2024-10-20 01:28:00 +0900

    [add]request spec

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,21 +1,28 @@
 class LikesController < ApplicationController
   before_action :set_practice_record, only: [:create, :destroy]
+  before_action :redirect_if_not_logged_in, only: [:create, :destroy]
 
   def index
 
   end
 
   def create
-    @like = current_user.likes.create!(practice_record: @practice_record)
+    @like = current_user.likes.new(practice_record: @practice_record)
 
-    respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace(
-          "like_button_#{@practice_record.id}",
-          partial: "shared/like_button",
-          locals: { practice_record: @practice_record, liked: true, like: @like }
-        )
+    if @like.save
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            "like_button_#{@practice_record.id}",
+            partial: "shared/like_button",
+            locals: { practice_record: @practice_record, liked: true, like: @like }
+          )
+        end
       end
+    else
+      flash.now[:alert] = "いいねに失敗しました"
+      flash.now[:alert_detail] = "時間をおいて再度お試しください"
+      render "practice_records/show", status: :unprocessable_entity
     end
   end
 
--- spec/requests/likes_controller_spec.rb
+++ spec/requests/likes_controller_spec.rb
@@ -3,9 +3,93 @@ require "rails_helper"
 RSpec.describe LikesController, type: :request do
   describe "POST /practice_records/:id/likes" do
 
+    context "ログインしている場合" do
+      let(:user) { FactoryBot.create(:user, email: "hoge@user.com") }
+      let(:practice_record) { FactoryBot.create(:practice_record) }
+
+      before { post '/login', params: { email: user.email, password: 'password' } }
+
+      it "レスポンス200" do
+        post("/practice_records/#{practice_record.id}/likes", headers: { "ACCEPT" => "text/vnd.turbo-stream.html" })
+        expect(response.status).to eq 200
+      end
+
+      it "likeの数が1増加" do
+        expect {
+          post("/practice_records/#{practice_record.id}/likes", headers: { "ACCEPT" => "text/vnd.turbo-stream.html" })
+        }.to change(Like, :count).by(1)
+      end
+
+      context "すでにlikeしている場合" do
+        let!(:like) { FactoryBot.create(:like, user: user, practice_record: practice_record) }
+
+        it "raise ActiveRecord::RecordNotUnique" do
+          expect {
+            post("/practice_records/#{practice_record.id}/likes", headers: { "ACCEPT" => "text/vnd.turbo-stream.html" })
+          }.to raise_error(ActiveRecord::RecordNotUnique)
+        end
+      end
+    end
+
+    context "ログインしていない場合" do
+      let(:user) { FactoryBot.create(:user, email: "hoge@user.com") }
+      let(:practice_record) { FactoryBot.create(:practice_record) }
+
+      it "likeの作成に失敗" do
+        post("/practice_records/#{practice_record.id}/likes", headers: { "ACCEPT" => "text/vnd.turbo-stream.html" })
+        expect(response.status).to eq 302
+      end
+
+      it "likeの数が変化しない" do
+        expect {
+          post("/practice_records/#{practice_record.id}/likes", headers: { "ACCEPT" => "text/vnd.turbo-stream.html" })
+        }.to_not change(Like, :count)
+      end
+
+      it "flash表示" do
+        post("/practice_records/#{practice_record.id}/likes", headers: { "ACCEPT" => "text/vnd.turbo-stream.html" })
+        expect(flash[:alert]).to eq "ログインしてください"
+      end
+    end
   end
 
   describe "DELETE /practice_records/:id/likes/:id" do
+
+    context "ログインしている場合" do
+      let(:user) { FactoryBot.create(:user, email: "hoge@user.com") }
+      before { post '/login', params: { email: user.email, password: 'password' } }
+
+      let(:practice_record) { FactoryBot.create(:practice_record) }
+      let!(:like) { FactoryBot.create(:like, user: user, practice_record: practice_record) }
+
+      it "レスポンス200" do
+        delete("/practice_records/#{practice_record.id}/likes/#{like.id}", headers: { "ACCEPT" => "text/vnd.turbo-stream.html" })
+        expect(response.status).to eq 200
+      end
+
+      it "likeの数が1減少" do
+        expect {
+          delete("/practice_records/#{practice_record.id}/likes/#{like.id}", headers: { "ACCEPT" => "text/vnd.turbo-stream.html" })
+        }.to change(Like, :count).by(-1)
+      end
+    end
+
+    context "ログインしていない場合" do
+      let(:user) { FactoryBot.create(:user, email: "hoge@user.com") }
+      let(:practice_record) { FactoryBot.create(:practice_record) }
+
+      it "レスポンス302" do
+        delete("/practice_records/#{practice_record.id}/likes/1", headers: { "ACCEPT" => "text/vnd.turbo-stream.html" })
+        expect(response.status).to eq 302
+      end
+
+      it "likeの数が変化しない" do
+        expect {
+          delete("/practice_records/#{practice_record.id}/likes/1", headers: { "ACCEPT" => "text/vnd.turbo-stream.html" })
+        }.to_not change(Like, :count)
+      end
+    end
+
 
   end
 end
